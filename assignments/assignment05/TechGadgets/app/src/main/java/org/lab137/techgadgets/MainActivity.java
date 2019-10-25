52fb6adca88cceb84349929227705eb9cb3c77f8
jay ess <mentoce@gmail.com>
2019-10-25 13:07:32 -0700

    Display list

--- assignments/assignment05/TechGadgets/app/src/main/java/org/lab137/techgadgets/MainActivity.java
+++ assignments/assignment05/TechGadgets/app/src/main/java/org/lab137/techgadgets/MainActivity.java
@@ -2,13 +2,30 @@ package org.lab137.techgadgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        String[] products = {
+            "Toshiba - 50” Class – Fire TV Edition",
+            "DJI - Mavic 2 Pro Quadcopter",
+            "Apple - MacBook Air - 13.3\"",
+            "Apple - 11-Inch iPad Pro (64GB)",
+            "Beats Solo3 Wireless Headphones"
+        };
+
+        setListAdapter(
+            new ArrayAdapter<String>(
+                this,
+                R.layout.activity_main,
+                R.id.product,
+                products)
+        );
     }
 }
