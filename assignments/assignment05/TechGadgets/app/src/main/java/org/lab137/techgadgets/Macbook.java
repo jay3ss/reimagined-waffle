100bfaffb638137e05ec42bf592929b1b35a6791
jay ess <mentoce@gmail.com>
2019-10-25 12:12:46 -0700

    Add activities/layouts for products

---
+++ assignments/assignment05/TechGadgets/app/src/main/java/org/lab137/techgadgets/Macbook.java
@@ -0,0 +1,14 @@
+package org.lab137.techgadgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Macbook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_macbook);
+    }
+}
