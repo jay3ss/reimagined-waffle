70e2501a0d8b6d2264921ffd094321c20a95407b
jay ess <mentoce@gmail.com>
2019-11-11 15:19:53 -0800

    Initial main activity without much, well, activity

--- examples/EndangeredSpecies/app/src/main/java/org/lab137/endangeredspecies/MainActivity.java
+++ examples/EndangeredSpecies/app/src/main/java/org/lab137/endangeredspecies/MainActivity.java
@@ -3,12 +3,64 @@ package org.lab137.endangeredspecies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Integer[] Animals = {
+        R.drawable.eagle, R.drawable.elephant, R.drawable.gorilla,
+        R.drawable.panda, R.drawable.panther, R.drawable.polar
+    };
+    ImageView pic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView grid = findViewById(R.id.gridView);
+        final ImageView pic = findViewById(R.id.imgLarge);
+        grid.setAdapter(new ImageAdapter(this));
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(
+                    getBaseContext(),
+                    "Selected Species " + (position + 1),
+                    Toast.LENGTH_SHORT)
+                .show();
+                pic.setImageResource(Animals[position]);
+            }
+        });
+    }
+
+    private class ImageAdapter extends BaseAdapter {
+        public ImageAdapter(MainActivity mainActivity) {
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }
