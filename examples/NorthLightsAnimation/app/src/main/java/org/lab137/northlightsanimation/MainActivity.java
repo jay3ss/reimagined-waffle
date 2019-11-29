3d3bb0caa066a0da2e82dcc6ecdf294a7913bf57
jay ess <mentoce@gmail.com>
2019-11-29 15:27:40 -0800

    Add starting and stopping of animation

--- examples/NorthLightsAnimation/app/src/main/java/org/lab137/northlightsanimation/MainActivity.java
+++ examples/NorthLightsAnimation/app/src/main/java/org/lab137/northlightsanimation/MainActivity.java
@@ -2,13 +2,39 @@ package org.lab137.northlightsanimation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    AnimationDrawable lightsAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgFrame = findViewById(R.id.imgLights);
+        imgFrame.setBackgroundResource(R.drawable.animation);
+        lightsAnimation = (AnimationDrawable) imgFrame.getBackground();
+
+        Button btnStart = findViewById(R.id.btnStart);
+        Button btnStop = findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lightsAnimation.start();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lightsAnimation.stop();
+            }
+        });
     }
 }
