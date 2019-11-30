3f4e9b5c757b9de370c95e2867f8faf3f4e98a9e
jay ess <mentoce@gmail.com>
2019-11-29 21:40:29 -0800

    Finish app

--- assignments/assignment10/FlagsoftheWorld/app/src/main/java/org/lab137/flagsoftheworld/Tween.java
+++ assignments/assignment10/FlagsoftheWorld/app/src/main/java/org/lab137/flagsoftheworld/Tween.java
@@ -3,6 +3,8 @@ package org.lab137.flagsoftheworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Tween extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class Tween extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tween);
+        ImageView imgFadeaway = findViewById(R.id.imgTween);
+        imgFadeaway.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fadeaway));
     }
 }
