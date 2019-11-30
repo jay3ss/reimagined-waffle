a1cbad36de255a0dafc216ceb5696d1c8ad25fe4
jay ess <mentoce@gmail.com>
2019-11-29 16:23:32 -0800

    Finish Tween activity

--- examples/NorthLightsAnimation/app/src/main/java/org/lab137/northlightsanimation/Tween.java
+++ examples/NorthLightsAnimation/app/src/main/java/org/lab137/northlightsanimation/Tween.java
@@ -3,6 +3,8 @@ package org.lab137.northlightsanimation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Tween extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class Tween extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tween);
+        ImageView imgRotate = findViewById(R.id.imgTween);
+        imgRotate.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotation));
     }
 }
